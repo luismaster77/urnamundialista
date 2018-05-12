88da2d1f98f6ae880c7bb87f181c7ecf9008689b
luis eduardo <luisparedes@umariana.edu.co>
2018-05-12 18:28:18 -0500

    Primera fase prueba

---
+++ urnaMundialista/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url
+from urnaMundialista import views, models
+from django.contrib.auth.decorators import login_required
+
+urlpatterns = [
+    url(r'^$', login_required(views.base), name='base'),
+    url(r'^jugador/$', views.JugadorListView.as_view(), name='jugadores-list'),
+    url(r'^jugador/(?P<pk>\d+)/detail/$', views.JugadorDetailView.as_view(), name='jugador-detail'),
+    url(r'^jugador/(?P<pk>\d+)/update/$', views.JugadorUpdate.as_view(),name='jugadores-update'),
+    #Create
+    url(r'^jugadores/create/$', views.JugadorCreate.as_view(), name='jugadores-create'),
+    #Delete
+    url(r'^jugador/(?P<pk>\d+)/delete/$', views.JugadorDelete.as_view(), name='jugadores-delete'),
+    #Reservar
+    #url(r'^jugador/votado/$', views.JugadorVotado.as_view(), name='votar'),
+]
---
+++ urnaMundialista/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.views.generic import ListView, DetailView, UpdateView, CreateView, DeleteView
+from urnaMundialista.models import Jugadores
+from django.core.urlresolvers import reverse_lazy
+
+# Create your views here.
+@login_required
+def base(request):
+    return render(request , "base.html")
+
+def jugador_list(request):
+    jugador_list = Jugador.objects.all()
+    context = {'object_list': jugador_list}
+    template_name='urnaMundialista/jugador_detail.html'
+    return render(request,'urnaMundialista/jugadores_list.html', context)
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls):
+        return login_required(super(LoginRequiredMixin, cls).as_view())
+
+class JugadorListView(LoginRequiredMixin, ListView):
+    model = Jugadores
+
+class JugadorDetailView(LoginRequiredMixin, DetailView):
+    model = Jugadores
+
+class JugadorUpdate(UpdateView):
+ login_required = True
+ model = Jugadores
+ fields = '__all__'
+
+class JugadorCreate(CreateView):
+ login_required = True
+ model = Jugadores
+ fields = '__all__'
+
+class JugadorDelete(DeleteView):
+ login_required = True
+ model = Jugadores
+ success_url = reverse_lazy('jugadores-list')
+
